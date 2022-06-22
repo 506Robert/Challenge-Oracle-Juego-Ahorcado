e04769d1dfeb942204e6359ad9309a86750bdf86
Robert <rgutier14261@gmail.com>
2022-06-22 15:21:41 -0600

    Challenge 2

---
+++ arrayLocalStorage.js
@@ -0,0 +1,3 @@
+var palabras = [ "AMARILLO","GATO","PANTALON","FLOR","CEREBRO","BOSQUE","GIRASOL","FUTBOL","CUADERNO","TRISTE","CORRER","ESCALERA","TORRE","CAMION"];
+
+localStorage.setItem('myArray', JSON.stringify(palabras));
